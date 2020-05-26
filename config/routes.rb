1a95eb39e906e3f5e28f863e2c4e987560fe9d37
Lynne <lynne@Kotiss-MacBook-Pro.local>
2020-05-25 17:33:14 -0700

    adds routes and controllers for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get '/routes', to: 'one_bus_away#routes'
+      get '/routes/:id/stops', to: 'one_bus_away#stops'
+      get '/stops/:id/arrivals', to: 'one_bus_away#arrivals'
+    end
+  end
 end
