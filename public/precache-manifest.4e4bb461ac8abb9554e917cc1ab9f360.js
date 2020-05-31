3ce52cc6e3d90f0cad2ddef3229c8e07373a9ccb
Lynne <ilynne@gmail.com>
2020-05-31 15:29:58 -0700

    builds the sample react front end

---
+++ public/precache-manifest.4e4bb461ac8abb9554e917cc1ab9f360.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e5690bacd25c0e3baa38200351ad7af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c77a0b5012d17426898",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "863a59ede416ea21c22c",
+    "url": "/static/js/2.770573d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.770573d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c77a0b5012d17426898",
+    "url": "/static/js/main.637dd6c9.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
